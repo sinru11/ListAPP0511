8724d3ed8f32ec0e8b6aab2f4b1c8299f9707427
sr <candy871121@gmail.com>
2020-05-11 12:32:45 +0800

    完成專案

--- app/src/main/java/com/u1065138/listapp/MainActivity.java
+++ app/src/main/java/com/u1065138/listapp/MainActivity.java
@@ -2,12 +2,37 @@ package com.u1065138.listapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] favorite;
+    private ListView ls;
+    private TextView txvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ls=findViewById(R.id.ls);
+        txvShow=findViewById(R.id.txvShow);
+        favorite=getResources().getStringArray(R.array.favorite);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this ,android.R.layout.simple_list_item_multiple_choice,favorite);
+        ls.setAdapter(ad);
+        ls.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+
+    }
+
+    public void btnOK(View view) {
+        String str ="";
+        for (int i=0;i<favorite.length;i++){
+            if(ls.isItemChecked(i))
+                str += favorite[i] +"\n";
+        }
+         txvShow.setText(str);
     }
 }
